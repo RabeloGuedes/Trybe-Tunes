f5f7af710470eb7ee232999478a246ece824d752
RabeloGuedes <krepscredian@gmail.com>
2022-04-19 13:37:45 -0300

    Adiciona ao Component 'Search' o campo para busca de artistas e seu respectivo botao.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentArtistName: '',
+      buttonState: true,
+    };
+    this.searchArtistName = this.searchArtistName.bind(this);
+  }
+
+  searchArtistName({ target }) {
+    this.setState(
+      () => ({ currentArtistName: target.value }),
+      () => this.changeButtonState(),
+    );
+  }
+
+  changeButtonState() {
+    const { currentArtistName } = this.state;
+    if (currentArtistName.length < 2) {
+      this.setState({ buttonState: true });
+    } else {
+      this.setState({ buttonState: false });
+    }
+  }
+
   render() {
+    const { currentArtistName, buttonState } = this.state;
     return (
       <div data-testid="page-search">
-        <h2>
-          Busca
-        </h2>
+        <form>
+          <input
+            type="text"
+            placeholder="Banda ou Artista"
+            data-testid="search-artist-input"
+            value={ currentArtistName }
+            onChange={ this.searchArtistName }
+          />
+          <button
+            type="button"
+            disabled={ buttonState }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
